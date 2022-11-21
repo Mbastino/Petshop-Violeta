f659c0ea62e50fdace73ddb41dbb1ea21f8f2214
Marcelo Bastino <marcelobastino@gmail.com>
2022-11-21 19:09:22 -0300

    utilizando .find

--- index.js
+++ index.js
@@ -1,8 +1,7 @@
-let productoSeleccionado = parseInt(prompt('Selecciona el producto que desee comprar: \n0.Dogui \n1.Royal Canin \n2.Dog Chow \n3.Ken-L'))
+let productoSeleccionado = parseInt(prompt('Selecciona el producto que desee comprar: \n0.Dogui $500 \n1.Royal Canin $600 \n2.Dog Chow $450 \n3.Ken-L $400'))
 let totalCompra = 0
-let seguiComprando = true
+let seguirComprando = true
 let decision
-
 
 class Producto{
     constructor(id,tipo,precio){
@@ -17,24 +16,23 @@ const royalCanin = new Producto(1,'Royal Canin',600)
 const dogChow = new Producto(2,'Dog Chow',450)
 const kenL = new Producto(3,'Ken-L',400)
 
-const comidasExistentes = [dogui,royalCanin,dogChow]
-comidasExistentes.push(kenL)
+const comidasExistentes = [dogui,royalCanin,dogChow,kenL]
 
-while (seguiComprando === true){
+while(seguirComprando === true){
     const productoCliente = comidasExistentes.find(producto=>producto.id === productoSeleccionado)
     if(productoCliente){
         totalCompra = totalCompra + productoCliente.precio
     } else{
-        productoSeleccionado = parseInt(prompt('Por favor seleccione uno de los productos de la lista: \n0.Dogui \n1.Royal Canin \n2.Dog Chow \n3.Ken-L'))
+        productoSeleccionado = parseInt(prompt('Por favor seleccione uno de los productos de la lista: \n0.Dogui $500 \n1.Royal Canin $600 \n2.Dog Chow $450 \n3.Ken-L $400'))
         continue
+    }
+    decision = parseInt(prompt('Quieres continuar comprando: \n1.Si \n2.No'))
+    if(decision === 1){
+        productoSeleccionado = parseInt(prompt('Selecciona el producto que desee comprar: \n0.Dogui $500 \n1.Royal Canin $600 \n2.Dog Chow $450 \n3.Ken-L $400'))
+    } else{
+        seguirComprando = false
     }
 }
 
-decision = parseInt(prompt('Quieres continuar comprando: \n1-Si \n2-No'))
-if(decision === 1){
-    let productoSeleccionado = parseInt(prompt('Selecciona el producto que desee comprar: \n0.Dogui \n1.Royal Canin \n2.Dog Chow \n3.Ken-L'))
-} else{
-    seguiComprando === false
-}
-
 alert(`El total de la compra es ${totalCompra}`)
+
