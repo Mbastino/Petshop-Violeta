2e9c80dad7f08cf067a818fd2a645e9f57681a1b
Marcelo Bastino <marcelobastino@gmail.com>
2022-11-26 15:44:29 -0300

    carrito en proceso

--- index.js
+++ index.js
@@ -36,3 +36,45 @@ while(seguirComprando === true){
 
 alert(`El total de la compra es ${totalCompra}`) */
 
+const contenedorProductos = document.getElementById('contenedor-productos')
+
+const contenedorCarrito = document.getElementById('carrito-contenedor')
+
+let carrito = []
+
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt="">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p class="precioProducto">Precio:${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="bi bi-cart"></i></button>
+    `
+    contenedorProductos.appendChild(div)
+
+    const boton = document.getElementById('agregar${producto.id}')
+    boton.addEventListener(click, ()=>{
+        agregarAlCarrito(producto.id)
+    })
+})
+
+const agregarAlCarrito = (prodId) => {
+    const item = stockProductos.find((prod)=> prod.id === prodId)
+    carrito.push(item)
+}
+ 
+const actualizarCarrito = () =>{
+    carrito.forEach((prod)=>{
+        const div = document.createElement('div')
+        div.className = ('productoEnCarrito')
+        div.innerHTML = `
+        <p>${prod.nombre}</p>
+        <p>Precio: ${prod.precio}</p>
+        <p>Cantidad: <span id="cantidad">${prod.cantidad},</span></p>
+        <button onclick = "eliminarDelCarrito(${prod.id})" class="boton-elminar"><i class="bi bi-trash"></i></button>
+        `
+        contenedorCarrito.appendChild(div)
+    })
+}
