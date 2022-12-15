38d900ff927485817d14c3c20aefff63eb69b3ee
Marcelo Bastino <marcelobastino@gmail.com>
2022-12-15 17:22:17 -0300

    finalizando proyecto

--- index.js
+++ index.js
@@ -32,45 +32,42 @@ botonVaciar.addEventListener('click', () => {
     actualizarCarrito()
 })
 
-fetch("stock.json")
-    .then(response => response.json())
-    .then(stockProductos => (
-        stockProductos.forEach((producto) => {
-            const div = document.createElement('div')
-            div.classList.add('producto')
-            div.innerHTML = `
-            <img src=${producto.img} alt= "">
-            <h3>${producto.nombre}</h3>
-            <p>${producto.desc}</p>
-            <p class="precioProducto">Precio:$ ${producto.precio}</p>
-            <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="bi bi-cart-fill"></i></i></button>
-            `
-            contenedorProductos.appendChild(div)
-           
-            const boton = document.getElementById(`agregar${producto.id}`)
-            
-            boton.addEventListener('click', () => {
-                Toastify({
-                    text: "Producto agregado al carrito",
-                    duration: 1500,
-                    destination: "",
-                    newWindow: true,
-                    close: false,
-                    gravity: "top",
-                    position: "center", 
-                    stopOnFocus: true, 
-                    style: {
-                      background: "linear-gradient(to right, #B279A7, #D387AB)",
-                    },
-                    onClick: function(){} 
-                  }).showToast();
         
-                agregarAlCarrito(producto.id)
-                
-            })
-        })
-    ))
+stockProductos.forEach((producto) => {
+    const div = document.createElement('div')
+    div.classList.add('producto')
+    div.innerHTML = `
+    <img src=${producto.img} alt= "">
+    <h3>${producto.nombre}</h3>
+    <p>${producto.desc}</p>
+    <p class="precioProducto">Precio:u$d ${producto.precio}</p>
+    <button id="agregar${producto.id}" class="boton-agregar">Agregar <i class="bi bi-cart-fill"></i></i></button>
+    `
+    contenedorProductos.appendChild(div)
+   
+    const boton = document.getElementById(`agregar${producto.id}`)
+    
 
+    boton.addEventListener('click', () => {
+        Toastify({
+            text: "Producto agregado al carrito",
+            duration: 1500,
+            destination: "",
+            newWindow: true,
+            close: false,
+            gravity: "top",
+            position: "center", 
+            stopOnFocus: true, 
+            style: {
+              background: "linear-gradient(to right, #B279A7, #D387AB)",
+            },
+            onClick: function(){} 
+          }).showToast();
+
+        agregarAlCarrito(producto.id)
+        
+    })
+}) 
 
 
 const agregarAlCarrito = (prodId) => {
@@ -109,7 +106,7 @@ const actualizarCarrito = () => {
         div.className = ('productoEnCarrito')
         div.innerHTML = `
         <p>${prod.nombre}</p>
-        <p>Precio:$${prod.precio}</p>
+        <p>Precio:u$d${prod.precio}</p>
         <p>Cantidad: <span id="cantidad">${prod.cantidad}</span></p>
         <button onclick="eliminarDelCarrito(${prod.id})" class="boton-eliminar"><i class="bi bi-trash-fill"></i></button>
         `
@@ -120,6 +117,14 @@ const actualizarCarrito = () => {
 
     })
     contadorCarrito.innerText = carrito.length 
-    console.log(carrito)
     precioTotal.innerText = carrito.reduce((acc, prod) => acc + prod.cantidad * prod.precio, 0)
 }
+const divDolar = document.getElementById("divDolar")
+
+fetch("https://criptoya.com/api/dolar")
+.then(response=>response.json())
+.then(({blue}) =>{
+    divDolar.innerHTML=`
+    <h3>La cotizacion del dolar al dia de la fecha es de : $${blue}</h3>
+    `
+})
---
+++ stock.js
@@ -0,0 +1,17 @@
+let stockProductos = [
+    /* Alimentos perros */
+    {id: 1, nombre: "Proplan", tipo: "alimento perro", cantidad: 1, desc: "Alimento Pro Plan OptiStart Puppy para perro cachorro de raza grande sabor pollo en bolsa de 18 kg", precio: 52.8, img: './img/ap/apProplancrg.jpg'},
+    {id: 2, nombre: "Proplan", tipo: "alimento perro", cantidad: 1, desc: "Alimento Pro Plan Complete Puppy para perro cachorro de raza mediana sabor pollo y arroz en bolsa de 18 kg", precio: 52.66, img: './img/ap/apProplancrm.jpg'},
+    {id: 3, nombre: "Excellent", tipo: "alimento perro", cantidad: 1, desc: "Alimento Excellent para perro cachorro de raza mediana y grande sabor pollo y arroz en bolsa de 22 kg", precio: 40, img: './img/ap/apExcellentcrm.jpg'},    
+    {id: 4, nombre: "Excellent", tipo: "alimento perro", cantidad: 1, desc: "Alimento Excellent para perro cachorro de raza pequeña sabor pollo y arroz en bolsa de 15 kg", precio: 31.33, img: './img/ap/apExcellentcrp.jpg'},
+    {id: 5, nombre: "Dog Chow", tipo: "alimento perro", cantidad: 1, desc: "Alimento Dog Chow Salud Visible cachorro de raza mini y pequeña sabor mix en bolsa de 21 kg", precio: 26.16, img: './img/ap/apDogchowcrp.jpg'},
+    {id: 6, nombre: "Kongo", tipo: "alimento perro", cantidad: 1, desc: "Kongo Gold Super Premiun Cachorros 21kgs", precio: 28.33, img: './img/ap/apKongocrp.jpg'}, 
+
+    /*Alimentos gatos*/
+    {id: 7, nombre: "Cat Chow", tipo: "alimento gato", cantidad: 1, desc: "Alimento Cat Chow Defense Plus para gato adulto sabor carne en bolsa de 15 kg", precio: 36, img: './img/ag/agCatchowa.jpg'}, 
+    {id: 8, nombre: "Cat Chow", tipo: "alimento gato", cantidad: 1, desc: "Alimento Cat Chow Defense Plus para gato de temprana edad sabor mix en bolsa de 15 kg", precio: 41.16, img: './img/ag/agCatchowc.jpg'}, 
+    {id: 9, nombre: "Proplan", tipo: "alimento gato", cantidad: 1, desc: "Alimento Pro Plan Optiprebio Adult para gato adulto sabor pollo y arroz en bolsa de 15 kg", precio: 70.8, img: './img/ag/agProplana.jpg'}, 
+    {id: 10, nombre: "Proplan", tipo: "alimento gato", cantidad: 1, desc: "Proplan Gato Kitten X 7.5 Kg", precio: 43.33, img: './img/ag/agProplanc.jpg'}, 
+    {id: 11, nombre: "Kongo", tipo: "alimento gato", cantidad: 1, desc: "Kongo Gato Adulto X 15 Kgs Carne Y Pollo", precio: 17.66, img: './img/ag/agKongoa.jpg'},
+    {id: 12, nombre: "Kongo", tipo: "alimento gato", cantidad: 1, desc: "Kongo Gatitos X 8 Kgs Carne Y Leche", precio: 12.66, img: './img/ag/agKongoc.jpg'}, 
+]
